6bf2c5e42bf29d005442a587f012e16033551472
Derek Ye <derek@mercury.com>
2024-11-24 22:56:46 -0800

    Add gitignore to remove packages and parse ChaseTransactions

--- app/Main.hs
+++ app/Main.hs
@@ -1,37 +1,64 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import Data.Time.Clock (UTCTime)
-import Data.Text (Text)
+import Data.Time (Day, fromGregorian)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.IO as TIO
 import qualified Data.Vector as V
 import Data.Csv
+import Debug.Trace
 
 data Transaction = ChaseTransaction
 
 data ChaseTransaction = MkChaseTransaction {
-    transactionDate :: UTCTime,
-    postDate :: UTCTime,
-    description :: Text,
-    category :: Text,
+    transactionDate :: Day,
+    postDate :: Day,
+    description :: T.Text,
+    category :: T.Text,
     transactionType :: ChaseCardTransactionType,
     amount :: Double,
-    memo :: Text
+    memo :: T.Text
 } deriving (Show)
 
-instance FromNamedRecord ChaseTransaction
-instance ToNamedRecord ChaseTransaction
-instance DefaultOrdered ChaseTransaction
+instance FromNamedRecord ChaseTransaction where
+    parseNamedRecord r = MkChaseTransaction 
+        <$> parseDateField "Transaction Date" r
+        <*> parseDateField "Post Date" r
+        <*> r .: "Description"
+        <*> r .: "Category"
+        <*> parseNamedRecord r  -- type
+        <*> r .: "Amount"
+        <*> r .: "Memo"
+
+instance FromNamedRecord ChaseCardTransactionType where
+    parseNamedRecord r = do
+        txnType <- r .: "Type"
+        case txnType of
+            "Credit" -> pure Credit
+            "Debit" -> pure Debit
+            _ -> fail $ "Unknown type: " ++ txnType
+
+parseDateField :: T.Text -> NamedRecord -> Parser Day
+parseDateField fieldName r = do
+    let fieldNameBS = TE.encodeUtf8 fieldName
+    txnDate <- r .: fieldNameBS :: Parser T.Text
+    pure $ fromGregorian 2024 3 15
 
 data ChaseCardTransactionType = Credit | Debit deriving (Show)
 
 main :: IO ()
 main = do
     result <- readCSV "chase-example.csv"
-    case result of
-        Left err -> putStrLn $ "Error: " ++ err
-        Right (_, v) -> V.forM_ v print
+    pure ()
 
-readCSV :: FilePath -> IO (Either String (V.Vector Transaction))
+readCSV :: FilePath -> IO ()
 readCSV fileName = do
     csvData <- BL.readFile fileName
-    return $ decodeByName csvData
+    case decodeByName csvData of
+        Left err -> putStrLn "wtf"
+        Right (_, v) -> V.forM_ v $ \ txn ->
+            putStrLn (T.unpack $ description txn <> " - " <> T.show (transactionDate txn))
+
