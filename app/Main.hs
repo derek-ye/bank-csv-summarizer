bf67bf2d800dd553233418f37f74da60155d9ea2
Derek Ye <derek@mercury.com>
2024-11-24 23:22:03 -0800

    Add faulty code to talk about

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,8 @@ import qualified Data.Text.IO as TIO
 import qualified Data.Vector as V
 import Data.Csv
 import Debug.Trace
+import Text.Read (readMaybe)
+import qualified Data.Text.Read as TR
 
 data Transaction = ChaseTransaction
 
@@ -25,8 +27,8 @@ data ChaseTransaction = MkChaseTransaction {
 
 instance FromNamedRecord ChaseTransaction where
     parseNamedRecord r = MkChaseTransaction 
-        <$> parseDateField "Transaction Date" r
-        <*> parseDateField "Post Date" r
+        <$> parseChaseDateField "Transaction Date" r
+        <*> parseChaseDateField "Post Date" r
         <*> r .: "Description"
         <*> r .: "Category"
         <*> parseNamedRecord r  -- type
@@ -41,11 +43,20 @@ instance FromNamedRecord ChaseCardTransactionType where
             "Debit" -> pure Debit
             _ -> fail $ "Unknown type: " ++ txnType
 
-parseDateField :: T.Text -> NamedRecord -> Parser Day
-parseDateField fieldName r = do
-    let fieldNameBS = TE.encodeUtf8 fieldName
-    txnDate <- r .: fieldNameBS :: Parser T.Text
-    pure $ fromGregorian 2024 3 15
+-- Chase dates are in the format YYYY/MM/DD
+parseChaseDateField :: T.Text -> NamedRecord -> Parser Day
+parseChaseDateField fieldName r = do
+        let fieldNameBS = TE.encodeUtf8 fieldName
+        -- why can't i do `txnDate :: T.Text <- ...`?
+        txnDate <- r .: fieldNameBS :: Parser T.Text
+        [year, month, day] <- parseInt <$> T.splitOn "/" txnDate      -- ["YYYY", "MM", "DD"]
+        pure $ fromGregorian year month day
+    where
+        parseInt :: T.Text -> Int
+        parseInt dateTxt =
+            case TR.decimal dateTxt of
+                Right (n, _) -> n -- success case, use n :: Int
+                Left err -> fail $ "Unknown date format: " ++ err -- handle error case
 
 data ChaseCardTransactionType = Credit | Debit deriving (Show)
 
