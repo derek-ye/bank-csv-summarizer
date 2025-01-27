c87f64eb6d44b5f502c459c0eae6a41ac6f555bc
Derek Ye <derek@mercury.com>
2025-01-27 13:46:52 -0800

    Use dotenv and actually categorize transactions

--- src/Core/Categorizer.hs
+++ src/Core/Categorizer.hs
@@ -12,20 +12,25 @@ import OpenAI.V1.Chat.Completions
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
 import qualified System.Environment as Environment
+import Configuration.Dotenv
 
 main2 :: IO ()
 main2 = do
+    -- loads .env file into environment
+    loadFile defaultConfig
     key <- Environment.getEnv "OPENAI_KEY"
 
     clientEnv <- getClientEnv "https://api.openai.com"
 
     let Methods{ createChatCompletion } = makeMethods clientEnv (Text.pack key)
 
-    text <- Text.IO.getLine
+    let text = "You are a high-performant system that banks use to categorize credit and debit card transactions. Given the options [Food & drink, Entertainment, General merchandise, General services, Payment, Personal care, Rent & utilities, Transportation, Travel], please categorize this transaction concisely: 'CYBERSPACE SUBSCRIPTION'"
+    -- text <- Text.IO.getLine
 
     ChatCompletionObject{ choices } <- createChatCompletion _CreateChatCompletion
         { messages = [ User{ content = [ Text{ text } ], name = Nothing } ]
         , model = "gpt-4o-mini"
+        , temperature = Just 0
         }
 
     let display Choice{ message } = Text.IO.putStrLn (messageToContent message)
