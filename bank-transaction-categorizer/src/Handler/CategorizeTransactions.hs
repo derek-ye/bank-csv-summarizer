f7e5ce02ebacbde226260bb747e9ef25f598b8ca
Derek Ye <derek@mercury.com>
2025-02-04 11:48:00 -0800

    Make bytestring work

--- bank-transaction-categorizer/src/Handler/CategorizeTransactions.hs
+++ bank-transaction-categorizer/src/Handler/CategorizeTransactions.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Handler.CategorizeTransactions where
 
 import Import
 import GHC.Generics
-import Data.Aeson (object)
 import qualified Data.Map as Map
+import Data.Aeson
+import qualified Data.Conduit as C
+import qualified Data.Conduit.List as CL
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy.UTF8 as LBS8
+import qualified Data.ByteString.Lazy as LBS
 
 newtype CategorizeTransactionsResult = CategorizeTransactionsResult {
     categorizedTransactions :: Map.Map Text Text
@@ -15,6 +21,10 @@ instance ToJSON CategorizeTransactionsResult
 
 postCategorizeTransactionsR :: Handler Value
 postCategorizeTransactionsR = do
+    bytes <- rawRequestBody C.$$ CL.fold BS.append BS.empty
+    let bodyStr = LBS8.toString $ LBS.fromStrict bytes
+    _ <- Import.traceM bodyStr
+
     pure $ toJSON result 
     where
         result = CategorizeTransactionsResult { categorizedTransactions = Map.fromList [("key1", "Food"), ("key2", "Groceries")] }
