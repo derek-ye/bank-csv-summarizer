f4f44d658cbc8ce655b7bfdefe3ce5d8da255c9d
Derek Ye <derek@mercury.com>
2025-04-27 17:04:27 -0700

    Fix multi-csv categorization

--- src/TransactionCategorizer/Core/Categorizer.hs
+++ src/TransactionCategorizer/Core/Categorizer.hs
@@ -42,11 +42,21 @@ categorizeTransactions key transactions = do
         , model = "gpt-4o-mini"
         , temperature = Just 0  -- we want there to be as little variance as possible between predictions
         }
-
-    pure $ chatCompletionToTextArr choices
+    let categorizedTransactions = parseOpenAiResponse $ chatCompletionToTextArr choices   -- looks like ["[Entertainment, General services, Transportation, Entertainment, Personal care, Travel, Entertainment, General merchandise, Entertainment, General services, Travel, General services, Food & drink, Food & drink, Food & drink, Food & drink, General merchandise, Rent & utilities, Payment, General merchandise]"]
+    pure $ categorizedTransactions
 
     where
-        promptText = "You are a high-performant system that banks use to categorize credit and debit card transactions. Given the options [Food & drink, Entertainment, General merchandise, General services, Payment, Personal care, Rent & utilities, Transportation, Travel], please categorize this list of transactions and return it as a single word: '" <> commaJoin transactions <> "'"
+        promptText = "You are a high-performant system that banks use to categorize credit and debit card transactions. Given the options [Food & drink, Entertainment, General merchandise, General services, Payment, Personal care, Rent & utilities, Transportation, Travel], please categorize this list of transactions and return it as list in the following format: [category for transaction at index 0, ...]. Go ahead: '" <> commaJoin transactions <> "'"
         chatCompletionToTextArr :: V.Vector Choice -> V.Vector T.Text
         chatCompletionToTextArr choices = messageToContent . message <$> choices
         commaJoin = T.intercalate ","
+
+        parseOpenAiResponse :: V.Vector T.Text -> V.Vector T.Text
+        parseOpenAiResponse s = commaSplit $ grabMiddle $ V.head s
+            where
+                -- Grab all elements except first and last
+                grabMiddle :: T.Text -> T.Text
+                grabMiddle txt = T.drop 1 (T.take (T.length txt - 1) txt)
+
+                commaSplit :: T.Text -> V.Vector T.Text
+                commaSplit t = V.fromList $ T.splitOn "," t
